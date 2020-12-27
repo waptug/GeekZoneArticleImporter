67986bff65a365d6bc32e31a2af3add79cee249f
waptug <waptug@gmail.com>
2020-12-27 09:15:59 -0800

    Created stand alone folder for non plugin example.

--- Stand_Alone_Solution/GeekZoneArticleImporter.php
+++ Stand_Alone_Solution/GeekZoneArticleImporter.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Operations of the plugin are included here. 
- *
- * @since 1.0
- */
-
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
-//
-
-
 /*
 try {
   echo $thisVariableIsNotSet;
